4b18d805a0b368eba231ea91e9e5c138583ebab4
jamate <mateoarcilalopez@gmail.com>
2023-10-02 14:27:13 -0500

    add new metric configurations for file styles

--- code/file_style_configs_by_metric.py
+++ code/file_style_configs_by_metric.py
@@ -4,12 +4,36 @@ file_style_a_details_factory = FileStyleDetailsFactory("%b-%Y", "A1", " | ", "A1
 file_style_b_details_factory = FileStyleDetailsFactory("%d-%m-%Y", "B2", " (", "A1", "A14", "B15")
 
 file_style_configs_by_metric = {
-    "ROA": {
+    "Pretax ROA": {
         FileStyle.A: file_style_a_details_factory.create("Pretax ROA", "Ratios - Key Metric"),
         FileStyle.B: file_style_b_details_factory.create("Pretax ROA", "Financial Summary")
     },
     "Gross Margin": {
         FileStyle.A: file_style_a_details_factory.create("Gross Margin", "Ratios - Key Metric"),
         FileStyle.B: file_style_b_details_factory.create("Gross Profit Margin", "Financial Summary")
-    }
+    },
+    "EBITDA Margin": {
+        FileStyle.A: file_style_a_details_factory.create("EBITDA Margin", "Ratios - Key Metric"),
+        FileStyle.B: file_style_b_details_factory.create("EBITDA Margin", "Financial Summary")
+    },
+    "Operating Margin": {
+        FileStyle.A: file_style_a_details_factory.create("Operating Margin", "Ratios - Key Metric"),
+        FileStyle.B: file_style_b_details_factory.create("Operating Margin", "Financial Summary")
+    },
+    "Net Margin": {
+        FileStyle.A: file_style_a_details_factory.create("Net Margin", "Ratios - Key Metric"),
+        FileStyle.B: file_style_b_details_factory.create("Net Margin", "Financial Summary")
+    },
+    "Net Margin": {
+        FileStyle.A: file_style_a_details_factory.create("Net Margin", "Ratios - Key Metric"),
+        FileStyle.B: file_style_b_details_factory.create("Net Margin", "Financial Summary")
+    },
+    "Pretax ROE": {
+        FileStyle.A: file_style_a_details_factory.create("Pretax ROE", "Ratios - Key Metric"),
+        FileStyle.B: file_style_b_details_factory.create("Pretax ROE", "Financial Summary")
+    },
+    "ROIC": {
+        FileStyle.A: file_style_a_details_factory.create("ROIC", "Ratios - Key Metric"),
+        FileStyle.B: file_style_b_details_factory.create("Pretax ROE", "Financial Summary")
+    },
 }
